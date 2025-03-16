b6e21fb5e6a292faf3fa410d2e4c1148ce57c18f
michaelrrrxxz <mangaoang.michaelangelo23@gmail.com>
2025-03-16 18:28:28 +0800

    change laravel 11 to laravel 12

--- backend/routes/channels.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Broadcast;
-
-Broadcast::channel('user-registered', function () {
-    return true;
-});
--- backend/routes/web.php
+++ backend/routes/web.php
@@ -5,6 +5,3 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-
-
-route::view('/login','login');
